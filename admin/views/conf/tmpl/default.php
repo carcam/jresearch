77e33b259d6c0fe7a115b006f181bd0cbab60b9e
lgalarra <shamantobi@gmail.com>
2008-10-20 16:35:46 +0000

    Correction of version number in control panel

--- admin/views/conf/tmpl/default.php
+++ admin/views/conf/tmpl/default.php
@@ -73,7 +73,7 @@ defined('_JEXEC') or die('Restricted access'); ?>
 					</tr>
 					<tr>
 						<td width="20%"><?php echo JText::_('JRESEARCH_SOFTWARE_VERSION').': '; ?></td>
-						<td>Beta 2</td>
+						<td>1.0 Stable</td>
 					</tr>
 					<tr>
 						<td width="20%"><?php echo JText::_('JRESEARCH_SOFTWARE_COPYRIGHT').': '; ?></td>
