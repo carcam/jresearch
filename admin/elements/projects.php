5c4effac40b45815f3dbfc65467807ceac461b74
fl0tschi <fl0tschi@b2eac5a0-5f58-0410-9def-8a54901e6d2c>
2008-10-29 20:37:21 +0000

    Implemented new projects selection element for the projectslist frontend view.

--- admin/elements/projects.php
+++ admin/elements/projects.php
@@ -40,15 +40,42 @@ class JElementProjects extends JElement
 		$projects = $db->loadObjectList();
 		
 		$projectsOptions = array();
-		
-		$projectsOptions[0] = JHTML::_('select.option', 0, 'all');
-		
 		foreach($projects as $project)
 		{
 			$projectsOptions[] = JHTML::_('select.option', $project->id, $project->title);
 		}
 		
-		return JHTML::_('select.genericlist', $projectsOptions, $name, 'class="inputbox" size="10" multiple="multiple"', 'value', 'text', $value);
+		$fieldName = $control_name.'['.$name.']';
+		$doc = JFactory::getDocument();
+		
+		//Javascript
+		$script = 'function changeValue () {';
+		
+		$script .= 'var selBox = document.getElementById(\'projectslist\');';
+		$script .= 'var txtBox = document.getElementById(\''.$name.'\');';
+		$script .= 'var value = selBox.options[selBox.selectedIndex].value;';
+		
+		$script .= 'var valueIn = false;';
+		$script .= 'var values = txtBox.value.split(\',\');';
+		
+		$script .= 'for(var i=0;i<values.length;i++) { if(values[i] == value) { valueIn = true; break; } }';
+		
+		$script .= 'if(!valueIn){';
+		
+		$script .= 'if(txtBox.value == "0" || txtBox.value == "") { txtBox.value = value; }';
+		$script .= 'else { txtBox.value += \',\' + value; }';
+		
+		$script .= '}}';
+		
+		//Add script
+		$doc->addScriptDeclaration($script);
+		
+		//Generate element
+		$html = '<input type="text" name="'.$fieldName.'" id="'.$name.'" readonly="readonly" value="'.$value.'" maxlength="65535" /> ';
+		$html .= '<input type="button" name="resetbtn" id="resetbtn" value="Reset" onclick="document.getElementById(\''.$name.'\').value=\'0\';" /><br />';
+		$html .= JHTML::_('select.genericlist', $projectsOptions, 'projectslist', 'class="inputbox" size="5" onchange="changeValue();"', 'value', 'text');
+		
+		return $html;
 	}
 }
 ?>
