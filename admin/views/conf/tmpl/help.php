5c697ba023c19d63941e1a03c93855d456418810
fl0tschi <fl0tschi@b2eac5a0-5f58-0410-9def-8a54901e6d2c>
2009-01-23 23:38:31 +0000

    Update for help

--- admin/views/conf/tmpl/help.php
+++ admin/views/conf/tmpl/help.php
@@ -6,12 +6,12 @@
  */
 defined('_JEXEC') or die('Restricted access'); 
 
-$file = JPATH_COMPONENT_ADMINISTRATOR.DS.'views'.DS.'conf'.DS.'tmpl'.DS.$this->langprefix.'_help.html';
-// Load english help by default
-if(!file_exists($file))
-	include_once(JPATH_COMPONENT_ADMINISTRATOR.DS.'views'.DS.'conf'.DS.'tmpl'.DS.'en_GB_help.html');
-else
-	include_once($file);	
+$directory = JPATH_COMPONENT_ADMINISTRATOR.DS.'views'.DS.'conf'.DS.'tmpl';
 
+$default_file = $directory.DS.'en_GB_help.html';
+$file = $directory.DS.$this->langprefix.'_help.html';
+
+// Load english help by default
+include_once(((!file_exists($file)) ? $file : $default_file));
 ?>
 
