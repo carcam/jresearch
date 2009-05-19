5025000582fc232c26b67397137fc55df95cadad
fl0tschi <fl0tschi@b2eac5a0-5f58-0410-9def-8a54901e6d2c>
2009-05-19 16:20:21 +0000

    DEFINES update and added coverflow for facilities. Not tested so not stable. English language files should be checked for updates to update the specific language files.

--- site/views/staff/view.html.php
+++ site/views/staff/view.html.php
@@ -12,26 +12,6 @@
 // No direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
-if (!defined('PHP_EOL'))
-{
-    switch (strtoupper(substr(PHP_OS, 0, 3)))
-    {
-        // Windows
-        case 'WIN':
-            define('PHP_EOL', "\r\n");
-            break;
-
-        // Mac
-        case 'DAR':
-            define('PHP_EOL', "\r");
-            break;
-
-        // Unix
-        default:
-            define('PHP_EOL', "\n");
-    }
-}
-
 jimport( 'joomla.application.component.view');
 
 /**
@@ -123,8 +103,6 @@ class JResearchViewStaff extends JView
 	*/
     private function getImages(&$members)
     {
-    	global $mainframe;
-    	
     	$images = array();
     	$i=0;
     	
