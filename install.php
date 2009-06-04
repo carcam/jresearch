63f76733008aa722bf082273eff81b26fe0fdd33
fl0tschi <fl0tschi@b2eac5a0-5f58-0410-9def-8a54901e6d2c>
2009-06-04 15:27:05 +0000

    Found another JoomFish installation issue

--- install.php
+++ install.php
@@ -25,8 +25,8 @@ function com_install(){
 	
 	// Copy Joom!Fish content elements if Joom!Fish extension exists
 	$joomFishCheckFile = JPATH_SITE.'components'.DS.'com_joomfish'.DS.'joomfish.php';
-	$srcFolder = JPATH_SITE.DS.'administrator'.DS.'components'.DS.'com_joomfish'.DS.'contentelements';
-	$destFolder = JPATH_SITE.'components'.DS.'com_joomfish'.DS.'contentelements';
+	$srcFolder = JPATH_SITE.DS.'administrator'.DS.'components'.DS.'com_jresearch'.DS.'contentelements';
+	$destFolder = JPATH_SITE.'administrator'.DS.'components'.DS.'com_joomfish'.DS.'contentelements';
 	
 	//Install Joomfish elements
 	if(file_exists($joomFishCheckFile))
