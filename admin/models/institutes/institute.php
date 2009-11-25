3ea3016bb73a76923ecfc152c30813412aae5d99
fl0tschi <fl0tschi@b2eac5a0-5f58-0410-9def-8a54901e6d2c>
2009-11-25 07:53:47 +0000

    Models for institutes

---
+++ admin/models/institutes/institute.php
@@ -0,0 +1,35 @@
+<?php
+/**
+* @version		$Id$
+* @package		JResearch
+* @subpackage	Institutes
+* @copyright	Copyright (C) 2008 Florian Prinz.
+* @license		GNU/GPL
+* This file implements the institute model.
+*/
+
+jimport( 'joomla.application.component.model' );
+
+require_once(JPATH_COMPONENT_ADMINISTRATOR.DS.'models'.DS.'modelSingleRecord.php');
+require_once(JPATH_COMPONENT_ADMINISTRATOR.DS.'tables'.DS.'institute.php');
+
+class JResearchModelInstitute extends JResearchModelSingleRecord
+{
+	/**
+	* Returns the record with the id sent as parameter.
+	* @return 	object
+	*/
+	public function getItem($itemId)
+	{
+		$db =& JFactory::getDBO();
+		
+		$coop = new JResearchInstitute($db);
+		$result = $coop->load($itemId);
+		
+		if($result)
+			return $coop;
+		else
+			return null;	
+	}
+}
+?>
