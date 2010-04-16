4b309289b710fa338b873810bdb3b6b0f980eade
lgalarra <shamantobi@gmail.com>
2010-04-16 04:48:43 +0000

    Preparation for 1.2 Beta 2

--- src/admin/includes/defines.php
+++ src/admin/includes/defines.php
@@ -11,7 +11,7 @@ define('_PROJECT_IMAGE_MAX_HEIGHT_', 768);
 define('_MEMBER_IMAGE_MAX_WIDTH_', 1024);
 define('_MEMBER_IMAGE_MAX_HEIGHT_', 768);
 
-define('_JRESEARCH_VERSION_', '1.2.0');
+define('_JRESEARCH_VERSION_', '1.2 Beta 2');
 define('_JRESEARCH_BIBUTILS_VERSION_', '4.8 for Unix, 4.2 for Windows, 4.9 for Unix x86/64');
 
 if (!defined('PHP_EOL'))
