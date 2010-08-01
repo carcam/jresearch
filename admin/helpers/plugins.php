95bf6fffef119a95339e3b22ac9608d682440484
lgalarra <shamantobi@gmail.com>
2010-08-01 16:58:08 +0000

    1.2.0 Stable tag

---
+++ admin/helpers/plugins.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * @package JResearch
+ * @subpackage Helpers
+ * @author Luis Galárraga
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Helper function for J!Research plugins related functionalities.
+ *
+ */
+class JResearchPluginsHelper{
+
+	/**
+	 * Triggers the onBeforeExecuteJResearchTask event. It executes every subscribed plugin 
+	 * until finding one that decided to handle the request.
+	 * @return boolean True if any of the subscribed plugins has decided to handle the request
+	 * so the specified controller is ignored. 
+	 */
+	public static function onBeforeExecuteJResearchTask(){
+		$dispatcher = JDispatcher::getInstance();
+		$event = 'onBeforeExecuteJResearchTask';
+		$args = array ();
+		
+		/*
+		 * We need to iterate through all of the registered observers and
+		 * trigger the event for each observer that handles the event.
+		 */
+		foreach ($dispatcher->_observers as $observer)
+		{
+			if (is_array($observer)){
+				if ($observer['event'] == $event){
+					if (function_exists($observer['handler'])){
+						$result = call_user_func_array($observer['handler'], $args);
+						// It means the previous plugin handled the request, so the other ones
+						// are ignored
+						if($result) return $result;
+					}else{
+						JError::raiseWarning('SOME_ERROR_CODE', 'JDispatcher::trigger: Event Handler Method does not exist.', 'Method called: '.$observer['handler']);
+					}
+				}else{
+					continue;
+				}
+			}
+			elseif (is_object($observer))
+			{
+				/*
+				 * Since we have gotten here, we know a little something about
+				 * the observer.  It is a class type observer... lets see if it
+				 * is an object which has an update method.
+				 */
+				if (method_exists($observer, 'update'))
+				{
+					/*
+					 * Ok, now we know that the observer is both not an array
+					 * and IS an object.  Lets trigger its update method if it
+					 * handles the event and return any results.
+					 */
+					if (method_exists($observer, $event)){
+						$args['event'] = $event;
+						$result = $observer->update($args);
+						// It means the previous plugin handled the request, so the other ones
+						// are ignored
+						if($result) return $result;
+					}else{
+						continue;
+					}
+				}else{
+					/*
+					 * At this point, we know that the registered observer is
+					 * neither a function type observer nor an object type
+					 * observer.  PROBLEM, lets throw an error.
+					 */
+					JError::raiseWarning('SOME_ERROR_CODE', 'JDispatcher::trigger: Unknown Event Handler.', $observer );
+				}
+			}
+		}	
+		return false;
+	} 
+}
+
+?>
---
+++ site/views/projectslist/tmpl/default.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @package JResearch
+ * @subpackage Projects
+ * Default view for showing a list of projects
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access'); ?>
+<h2 class="componentheading"><?php echo JText::_('JRESEARCH_PROJECTS'); ?></h2>
+
+<?php
+if(count($this->items) > 0):
+?>
+<ul style="padding-left:0px;">
+ 
+<?php foreach($this->items as $project): ?>
+	<?php $researchArea = $this->areaModel->getItem($project->id_research_area); ?>
+	<li class="liresearcharea">
+		<div>
+			<?php $contentArray = explode('<hr id="system-readmore" />', $project->description); ?>
+			<?php $itemId = JRequest::getVar('Itemid'); ?>
+			<h3 class="contentheading"><?php echo $project->title; ?></h3>
+			<?php 
+			//Show research area?
+			if($this->params->get('show_researcharea') == 1):
+			?>		
+			<div>
+				<h4><?php echo JText::_('JRESEARCH_RESEARCH_AREA')?></h4>
+				<?php if($researchArea->id > 1): ?>
+					<span><a href="index.php?option=com_jresearch&amp;view=researcharea&amp;id=<?php echo $researchArea->id; ?><?php echo isset($itemId)?'&amp;Itemid='.$itemId:''; ?>"><?php echo $researchArea->name;  ?></a></span>
+				<?php else: ?>
+					<span><?php echo $researchArea->name;  ?></span>
+				<?php endif; ?>	
+			</div>
+			<?php 
+			endif;
+			
+			//Show members?
+			if($this->params->get('show_members') == 1):
+				$members = implode(', ',$project->getPrincipalInvestigators());
+			?>			
+			<div>
+        		<h4><?php echo JText::_('JRESEARCH_PROJECT_LEADERS')?></h4>
+        		<span><?php echo $members; ?></span>
+			</div>
+			<?php 
+			endif;
+			
+			//Get values and financiers for project
+                  	$financiers = implode(', ', $project->getFinanciers());
+          	
+			$value = str_replace(array(",00",".00"), ",-", $project->finance_value); //Replace ,/.00 with ,-
+			
+			//Convert value to format 1.000.000,xx
+			$aFloat = substr($value, strpos($value, ","));
+			$cValue = array_reverse(str_split(strrev(substr($value, 0, strpos($value, ","))), 3));
+			
+			$convertedArray = array();
+			foreach($cValue as $val)
+			{
+				$convertedArray[] = strrev($val);
+			}
+			
+			$value = implode(".",$convertedArray).$aFloat;
+			
+			if($project->finance_value > 0):
+			?>
+			<div>
+				<h4><?php echo JText::_('JRESEARCH_PROJECT_FUNDING')?></h4>
+				<span><?php echo $financiers ?></span>, <h4><?php echo $project->finance_currency." ".$value ?></h4>
+			</div>
+			<?php
+			endif;
+			
+			if(!empty($contentArray[0])):
+			?>
+				<div>&nbsp;</div>
+				<?php echo $contentArray[0]; ?>
+			<?php
+			endif;
+			?>
+			<div style="text-align:left">
+				<?php echo JHTML::_('jresearch.link', JText::_('JRESEARCH_READ_MORE'), 'project', 'show', $project->id); ?>
+			</div>
+		</div>
+
+	</li>	
+<?php endforeach; ?>
+</ul>
+
+<?php
+endif;
+?>
+<div style="text-align:center;"><?php echo $this->page->getResultsCounter(); ?><br /><?php echo $this->page->getPagesLinks(); ?></div>
