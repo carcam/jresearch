885a4fd82c7cc4d9f8c9352888ff307e342386a8
fl0tschi <fl0tschi@b2eac5a0-5f58-0410-9def-8a54901e6d2c>
2010-11-13 11:06:30 +0000

    Added mod_jresearch_papers for osteopathic research, need to be tested

---
+++ modules/mod_jresearch_papers/helper.php
@@ -0,0 +1,57 @@
+<?php
+/**
+* @package		Joomla
+* @copyright	Copyright (C) 2005 - 2008 Open Source Matters. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+/** ensure this file is being included by a parent file */
+defined('_JEXEC') or die('Direct Access to this location is not allowed.');
+
+class modJResearchPapersHelper
+{
+	/**
+	 * Gets cooperations from database
+	 *
+	 * @return array
+	 */
+	function getNewestPapers($limit=5)
+	{
+		$papers = array();
+		$limit = intval($limit,10);
+		$db = &JFactory::getDBO();
+		$query = "SELECT * FROM #__jresearch_publication WHERE published = 1 ORDER by created DESC LIMIT 0,$limit";
+		
+		$db->setQuery($query);
+		$result = $db->loadAssocList();
+		foreach($result as $item){
+			$pub = JTable::getInstance("Publication", "JResearch");
+			$pub->bind($item, array(), true);
+			$papers = $pub;
+		}
+		
+		return $papers;
+	}
+	
+	function getMostViewed($limit=5)
+	{
+		$papers = array();
+		$db = &JFactory::getDBO();
+		$query = "SELECT * FROM #__jresearch_publication WHERE published = 1 ORDER by hits DESC LIMIT 0,$limit";
+		
+		$db->setQuery($query);
+		$result = $db->loadAssocList();
+		foreach($result as $item){
+			$pub = JTable::getInstance("Publication", "JResearch");
+			$pub->bind($item, array(), true);
+			$papers = $pub;
+		}
+		
+		return $papers;
+	}
+}
