60d2102ad1db8ec596539896c6443f36a753798f
fl0tschi <fl0tschi@b2eac5a0-5f58-0410-9def-8a54901e6d2c>
2010-11-13 11:20:05 +0000

    Updated layout

--- modules/mod_jresearch_papers/tmpl/default.php
+++ modules/mod_jresearch_papers/tmpl/default.php
@@ -1,1 +1,12 @@
-<?php
+<div id="mod_jresearch_papers">
+	<h2><?php echo JText::_('JRESEARCH_PAPERS'); ?></h2>
+	<?php
+	foreach($papers as $paper):
+	?>
+	<ul class="listing">
+		<li><?php echo $paper->title.(($type == "most_viewed")?(" (".$paper->hits.")"):""); ?></li>
+	</ul>
+	<?php 
+	endforeach;
+	?>
+</div>
