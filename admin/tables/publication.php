7555b19e7a58a9c168da8878e81aa88f244b172f
fl0tschi <fl0tschi@b2eac5a0-5f58-0410-9def-8a54901e6d2c>
2010-12-16 15:28:59 +0000

    Constant update for table

--- admin/tables/publication.php
+++ admin/tables/publication.php
@@ -17,7 +17,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 
 jimport('joomla.utilities.date');
 
-require_once(JRESEARCH_COMPONENT_ADMIN.DS.'tables'.DS.'activity.php');
+require_once(JPATH_ADMINISTRATOR.'components'.DS.'com_jresearch'.DS.'tables'.DS.'activity.php');
 
 /**
  * This class defines the base for all types of publications managed by JResearch.
