6b17b51ce227b85d9f0e9955f39f40bcfb815954
Carlos M. <contact@cmcamara.com>
2014-02-23 17:26:03 +0000

    Fixed pubtype reference that was throwing an error when creating a publication

--- src/admin/views/publication/view.html.php
+++ src/admin/views/publication/view.html.php
@@ -51,7 +51,7 @@ class JResearchAdminViewPublication extends JResearchView
 			
         $pubtype = JRequest::getVar('pubtype', isset($data['pubtype']) ? $data['pubtype'] : null, 'jform');
         
-        $changeType = JHTML::_('jresearchhtml.publicationstypeslist', 'change_type', '', $data['pubtype']);
+        $changeType = JHTML::_('jresearchhtml.publicationstypeslist', 'change_type', '', $pubtype);
             
         $this->assignRef('form', $form);
         $this->assignRef('data', $data);
