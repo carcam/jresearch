bbbf81fd638eefe913ec058d721d79de3c6cb1a2
Carlos Cámara <cmcamara@gmail.com>
2015-08-02 16:38:45 +0200

    Added compatibility classes for j25

--- src/admin/includes/init.php
+++ src/admin/includes/init.php
@@ -48,4 +48,16 @@ $document = JFactory::getDocument();
 $url = JURI::root();
 $document->addStyleSheet($url.'administrator/components/com_jresearch/css/jresearch_admin_styles.css');
 
+//Loading Bootstrap for Joomla! 2.5
+if (version_compare(JVERSION, "3.0.0", 'le'))
+{
+	JLoader::register('JHtmlBootstrap',JPATH_LIBRARIES."/jresearch/bootstrap/bootstrap.php");
+	JLoader::register('JHtmlJquery',JPATH_LIBRARIES."/jresearch/bootstrap/jquery.php");
+	JLoader::register('JLayout',JPATH_LIBRARIES."/jresearch/jlayouts/layout.php");
+	JLoader::register('JLayoutHelper',JPATH_LIBRARIES."/jresearch/jlayouts/helper.php");
+	JLoader::register('JLayoutFile',JPATH_LIBRARIES."/jresearch/jlayouts/file.php");
+	JLoader::register('JLayoutBase',JPATH_LIBRARIES."/jresearch/jlayouts/base.php");
+
+}
+
 ?>
